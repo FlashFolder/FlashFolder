bd2fa6c139a64107bcc8653c03dc025c8288e88f
zett42 <zett42>
2007-05-06 11:14:56 +0000

    FIX: upgrading to WiX_CptUI v1.7 fixes installer compile issue
    git-svn-id: https://svn.code.sf.net/p/flashfolder/code/trunk@34 a429ba1a-122f-0410-b780-ea7094eed80e

--- Installer/build.bat
+++ Installer/build.bat
@@ -7,7 +7,7 @@ tools\MkVersionFile.exe ..\_version.h _version.wxi.templ _version.wxi
 if NOT "%ERRORLEVEL%"=="0" (
 	exit /B 1
 )
-%WIX_2_0_DIR%\light.exe -nologo mainscript.wixobj -loc ProductTexts_EN-US.wxl wix-cptui\Lib\CptUI_simple.wixlib -loc wix-cptui\wxs\Texts_EN-US.wxl -out msi\FlashFolder_setup.msi
+%WIX_2_0_DIR%\light.exe -nologo mainscript.wixobj -loc ProductTexts_EN-US.wxl wix-cptui\lib\CptUI_simple.wixlib -loc wix-cptui\lib\Texts_EN-US.wxl -out msi\FlashFolder_setup.msi
 if NOT "%ERRORLEVEL%"=="0" (
 	exit /B 1
 )
