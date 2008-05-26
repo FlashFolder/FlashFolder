8e3e81b2d44e337af24b63e2cd0036cb550f1bc5
zett42 <zett42>
2008-05-26 21:21:19 +0000

    ADD: Installer - Multi-User dialog
    git-svn-id: https://svn.code.sf.net/p/flashfolder/code/trunk@187 a429ba1a-122f-0410-b780-ea7094eed80e

--- Installer/build.bat
+++ Installer/build.bat
@@ -5,11 +5,11 @@ set CULTURE=en-US
 
 tools\MkVersionFile.exe ..\_version.h _version.wxi.templ _version.wxi
 
-"%WIX_3_0_DIR%\candle.exe" -nologo MainScript.wxs Features.wxs ProgramFiles.wxs Regkeys.wxs MyWixUI_InstallDir.wxs -dCulture=%CULTURE% -ext WixUiExtension
+"%WIX_3_0_DIR%\candle.exe" -nologo MainScript.wxs Features.wxs ProgramFiles.wxs Regkeys.wxs MyWixUI_InstallDir.wxs MultiUserDlg.wxs -dCulture=%CULTURE% -ext WixUiExtension
 if NOT "%ERRORLEVEL%"=="0" (
 	exit /B 1
 )
-"%WIX_3_0_DIR%\light.exe" -nologo MainScript.wixobj Features.wixobj ProgramFiles.wixobj Regkeys.wixobj MyWixUI_InstallDir.wixobj -loc ProductTexts_%CULTURE%.wxl -cultures:%CULTURE% -ext WixUtilExtension -ext WixUiExtension -out msi\FlashFolder_setup.msi
+"%WIX_3_0_DIR%\light.exe" -nologo MainScript.wixobj Features.wixobj ProgramFiles.wixobj Regkeys.wixobj MyWixUI_InstallDir.wixobj MultiUserDlg.wixobj -loc ProductTexts_%CULTURE%.wxl -cultures:%CULTURE% -ext WixUtilExtension -ext WixUiExtension -out msi\FlashFolder_setup.msi
 if NOT "%ERRORLEVEL%"=="0" (
 	exit /B 1
 )
