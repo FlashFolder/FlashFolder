bb8fd9edf97b57de700699d8ee275f78b6a21161
zett42 <zett42>
2008-06-01 23:00:15 +0000

    ADD: dialog for upgrade installations CHG: some minor installer GUI stuff
    git-svn-id: https://svn.code.sf.net/p/flashfolder/code/trunk@195 a429ba1a-122f-0410-b780-ea7094eed80e

--- Installer/build.bat
+++ Installer/build.bat
@@ -5,11 +5,11 @@ set CULTURE=en-US
 
 tools\MkVersionFile.exe ..\_version.h _version.wxi.templ _version.wxi
 
-"%WIX_3_0_DIR%\candle.exe" -nologo MainScript.wxs Features.wxs ProgramFiles.wxs Regkeys.wxs MyWixUI_InstallDir.wxs MultiUserDlg.wxs -dCulture=%CULTURE% -ext WixUiExtension
+"%WIX_3_0_DIR%\candle.exe" -nologo MainScript.wxs Features.wxs ProgramFiles.wxs Regkeys.wxs MyWixUI_InstallDir.wxs MyWixUI_WelcomeDlg.wxs MyWixUI_UpgradeDlg.wxs MyWixUI_MultiUserDlg.wxs -dCulture=%CULTURE% -ext WixUiExtension
 if NOT "%ERRORLEVEL%"=="0" (
 	exit /B 1
 )
-"%WIX_3_0_DIR%\light.exe" -nologo MainScript.wixobj Features.wixobj ProgramFiles.wixobj Regkeys.wixobj MyWixUI_InstallDir.wixobj MultiUserDlg.wixobj -loc ProductTexts_%CULTURE%.wxl -cultures:%CULTURE% -ext WixUtilExtension -ext WixUiExtension -out msi\FlashFolder_setup.msi
+"%WIX_3_0_DIR%\light.exe" -nologo MainScript.wixobj Features.wixobj ProgramFiles.wixobj Regkeys.wixobj MyWixUI_InstallDir.wixobj MyWixUI_WelcomeDlg.wixobj MyWixUI_UpgradeDlg.wixobj MyWixUI_MultiUserDlg.wixobj -loc ProductTexts_%CULTURE%.wxl -cultures:%CULTURE% -ext WixUtilExtension -ext WixUiExtension -out msi\FlashFolder_setup.msi
 if NOT "%ERRORLEVEL%"=="0" (
 	exit /B 1
 )
