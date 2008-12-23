973c726cda1366005c4e37077a899e62f3bb18dd
zett42 <zett42>
2008-12-23 20:38:25 +0000

    DEL: removed registry CA and custom verify-remove-page (so we always keep user settings) FIX: fixed minor code issues
    git-svn-id: https://svn.code.sf.net/p/flashfolder/code/trunk@224 a429ba1a-122f-0410-b780-ea7094eed80e

--- Installer/CustomActions/CustomActions.cpp
+++ Installer/CustomActions/CustomActions.cpp
@@ -20,8 +20,6 @@
 
 using namespace std;
 
-const WCHAR MY_PRODUCT_REG[] = L"Software\\zett42\\FlashFolder";
-
 //----------------------------------------------------------------------------------------------
 
 BOOL APIENTRY DllMain( HINSTANCE hModule, DWORD reason, LPVOID lpReserved )
@@ -41,49 +39,4 @@ UINT __stdcall MsgBox(MSIHANDLE hModule)
 {
 	MessageBox(NULL, _T("CustomAction \"MsgBox\" running"), _T("Installer"), MB_ICONINFORMATION);
 	return ERROR_SUCCESS;
-}
-
-//----------------------------------------------------------------------------------------------
-/// Immediate CA for adding temporary rows to Registry table so we can better decide WHEN 
-/// regkeys / regvalues should be created or removed. 
-///
-/// This way it is possible for instance to set a registry value only on first install but
-/// neither "repair" nor uninstall it (e. g. for changing existing system regvalues).
-///
-/// Another example is a conditional uninstall of a regkey (standard MSI evaluates conditions
-/// only during install / reinstall).
-///
-/// Using temporary rows is better than doing this stuff directly with the registry API since
-/// we don't need to care about rollback as MSI is doing this automatically.
-
-UINT __stdcall CA_CustomRegistry( MSIHANDLE hInst )
-{
-	HRESULT hResult = WcaInitialize( hInst, L"CA_CustomRegistry" );
-	if( FAILED( hResult ) ) return ERROR_INSTALL_FAILURE;
-	
-	// Get installation state
-	bool installed = HasMsiProp( L"Installed" ) != 0;
-	bool removeAll = GetMsiProp( L"REMOVE" ) == L"ALL";
-	
-	try
-	{
-		// On uninstall, remove user regkeys, if desired.
-		if( removeAll && ! HasMsiProp( L"MY_KEEP_SETTINGS" ) )	
-		{
-			WcaLog( LOGMSG_STANDARD, L"Scheduling removal of user settings" );
-			PMSIHANDLE hTable, hColumns;
-			
-			MyWcaAddTempRecordRegistry( &hTable, &hColumns,  
-				L"RemoveProductReg", msidbRegistryRootLocalMachine, MY_PRODUCT_REG, L"-", NULL, L"ProgramRegkeys" );			
-		}
-
-		WcaDumpTable( L"Registry" );
-
-		return WcaFinalize( ERROR_SUCCESS );
-	}
-	catch( const MyWcaError& e )
-	{
-		WcaLogError( e.GetHResult(), e.wwhat() );
-		return WcaFinalize( ERROR_INSTALL_FAILURE );	
-	}
 }
