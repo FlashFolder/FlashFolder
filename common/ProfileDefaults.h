ea841f0adf43c656add1efd16c1e133a357e134d
zett42 <zett42>
2012-01-07 22:37:11 +0000

    CHG: copyright notice of source files
    git-svn-id: https://svn.code.sf.net/p/flashfolder/code/trunk@278 a429ba1a-122f-0410-b780-ea7094eed80e

--- common/ProfileDefaults.h
+++ common/ProfileDefaults.h
@@ -1,5 +1,5 @@
 /* This file is part of FlashFolder. 
- * Copyright (C) 2007 zett42 ( zett42 at users.sourceforge.net ) 
+ * Copyright (C) 2007-2012 zett42.de ( zett42 at users.sourceforge.net ) 
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
